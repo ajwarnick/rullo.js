252a1108b79fafc9c215ab7c9e22e799bb5da856
Anthony Warnick <ajwarnick@gmail.com>
2020-11-05 15:15:19 -0600

    update generator

--- mjs/modules/generator.mjs
+++ mjs/modules/generator.mjs
@@ -18,12 +18,38 @@ generator.load = (size, mode) => {
 
     // this is an example matrix for a 5x5 grid
     let matrix = [7, 5, 4, 4, 1, 3, 3, 4, 8, 4, 4, 3, 5, 5, 3, 8, 6, 1, 1, 3, 8, 6, 5, 7, 7];
-
+    let punched = [];
+    console.log(generator.solve(5,punched));
+    
     // GENERATE AND CHECK IF MATRIX WORKS
 
     // this sends the gererated matrix to the main module for use in the game
     return matrix;
 }
 
+
+generator.solve = (size, matrix) => {
+    let rows = [];
+    let cols = [];
+
+    for (let i=0; i < size; i++) {
+        rows.push( generator.sum( matrix.slice( (i*size), (i*size)+size ) ) );
+        cols.push( generator.sum( generator.col(matrix, size, i) ) )
+    }
+
+    return { rows, cols };
+}
+
+// Sums all values in array
+generator.sum = (arr) => arr.reduce((a, b) => a + b, 0);
+
+// Extracts the columns of the matrix from the 1-dementional array
+generator.col = (arr, nth, start) => arr.filter((e, i) => i === start || i % nth === start);
+
+generator.punch = (arr) => {
+
+    // DELETE RANDOM INDEXES
+    return arr; 
+}
 
 export { generator };
